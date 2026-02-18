6c50ba7e3e8f46894b378d0361b32225374d38e4
Fahd Ashour <fahd.fady212@gmail.com>
2026-02-19 00:30:00 +0200

    ObjectFile struct & CLI interface

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
+#include <elf.h>
+#include <stddef.h>
 #include <stdio.h>
 
-int main() {
-  printf("Hello, world!\n");
+typedef struct {
+  char *file_name;
+  size_t size;
+  // elf headers
+  Elf64_Ehdr *ehdr;
+  // section headers
+  Elf64_Shdr *shdrs;
+} ObjectFile;
+
+ObjectFile load_object(const char *file_name) {
+  printf("%s", file_name);
+
+  // get file size
+
+  // get elf headers
+
+  // get section headers
+
+  ObjectFile obj_file;
+
+  return obj_file;
+};
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "usage %s <file_1> <file_2>", argv[0]);
+    return 1;
+  };
+
+  load_object("objectfile");
   return 0;
 }
