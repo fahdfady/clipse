7374f97928bb24b58c5b8e2c2db47a27d0e4913f
Fahd Ashour <fahd.fady212@gmail.com>
2026-02-20 00:52:13 +0200

    get file size, and delete irrelevant files

--- main.c
+++ main.c
@@ -11,16 +11,30 @@ typedef struct {
   Elf64_Shdr *shdrs;
 } ObjectFile;
 
-ObjectFile load_object(const char *file_name) {
-  printf("%s", file_name);
+long int get_file_size(const char *file_name) {
 
-  // get file size
+  FILE *fp = fopen(file_name, "r");
+
+  if (fp == NULL) {
+    printf("couldn't open file %s\n", file_name);
+    // return something ??
+    return -1;
+  }
+
+  // return size;
+
+  return fseek(fp, 0, SEEK_END);
+}
+
+ObjectFile load_object(const char *file_name) {
+  ObjectFile obj_file;
+
+  obj_file.file_name = file_name;
+  obj_file.size = get_file_size(file_name);
 
   // get elf headers
 
   // get section headers
-
-  ObjectFile obj_file;
 
   return obj_file;
 };
@@ -31,6 +45,7 @@ int main(int argc, char *argv[]) {
     return 1;
   };
 
-  load_object("objectfile");
+  load_object(argv[1]);
+  load_object(argv[2]);
   return 0;
 }
