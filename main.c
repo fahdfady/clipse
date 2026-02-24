3745182dd4ee5d94557b9bcff90f29ea9434c833
Fahd Ashour <fahd.fady212@gmail.com>
2026-02-24 06:25:30 +0200

    get section headers and print some info. fix unclosed fd.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 typedef struct {
   void *data;
@@ -29,11 +30,23 @@ long int get_file_size(const char *file_name) {
   return size;
 }
 
-void print_elf_header(Elf64_Ehdr *ehdr) {
+void print_elf_debug_info(Elf64_Ehdr *ehdr, Elf64_Shdr *shdrs) {
   printf("\n=== ELF Header ===\n");
   printf("type: %hu\n", ehdr->e_type);
   printf("machine: %hu\n", ehdr->e_machine);
   printf("sections number: %hu\n", ehdr->e_shnum);
+
+  printf("\n=== Section Header ===\n");
+  printf("e_shoff = %lu (section headers at byte %lu)\n", ehdr->e_shoff,
+         ehdr->e_shoff);
+  printf("e_shnum = %u (there are %u sections)\n", ehdr->e_shnum,
+         ehdr->e_shnum);
+  printf("e_shstrndx = %u (section %u has names)\n", ehdr->e_shstrndx,
+         ehdr->e_shstrndx);
+
+  // change this into the acutal name
+  int first_section_name = shdrs[1].sh_name;
+  printf("Section 1 name offset: %d\n", first_section_name);
 }
 
 ObjectFile load_object(const char *file_name) {
@@ -52,11 +65,12 @@ ObjectFile load_object(const char *file_name) {
 
   obj_file.data = mmap(NULL, obj_file.size, PROT_READ, MAP_PRIVATE, fd, 0);
 
-  obj_file.ehdr = (Elf64_Ehdr *)obj_file.data;
-  print_elf_header(obj_file.ehdr);
-  printf("%s: %zu", obj_file.file_name, obj_file.size);
+  close(fd);
 
-  // get section headers
+  obj_file.ehdr = (Elf64_Ehdr *)obj_file.data;
+
+  // map the data with the ehdr pointing us to the section header offset
+  obj_file.shdrs = (Elf64_Shdr *)(obj_file.data + obj_file.ehdr->e_shoff);
 
   return obj_file;
 }
@@ -67,7 +81,13 @@ int main(int argc, char *argv[]) {
     return 1;
   };
 
-  load_object(argv[1]);
-  load_object(argv[2]);
+  printf("Loading %s...\n", argv[1]);
+  ObjectFile obj1 = load_object(argv[1]);
+  print_elf_debug_info(obj1.ehdr, obj1.shdrs);
+
+  printf("Loading %s...\n", argv[2]);
+  ObjectFile obj2 = load_object(argv[2]);
+  print_elf_debug_info(obj2.ehdr, obj2.shdrs);
+
   return 0;
 }
