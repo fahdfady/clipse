19f79e1846c3650a51d4610ad38fbf76df7356e1
Fahd Ashour <fahd.fady212@gmail.com>
2026-02-24 05:57:51 +0200

    access & print to debug some info from the elf header

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <elf.h>
-#include <stddef.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
 typedef struct {
+  void *data;
   char *file_name;
   size_t size;
   // elf headers
@@ -12,7 +15,6 @@ typedef struct {
 } ObjectFile;
 
 long int get_file_size(const char *file_name) {
-
   FILE *fp = fopen(file_name, "r");
 
   if (fp == NULL) {
@@ -20,24 +22,44 @@ long int get_file_size(const char *file_name) {
     // return something ??
     return -1;
   }
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  fclose(fp);
 
-  // return size;
+  return size;
+}
 
-  return fseek(fp, 0, SEEK_END);
+void print_elf_header(Elf64_Ehdr *ehdr) {
+  printf("\n=== ELF Header ===\n");
+  printf("type: %hu\n", ehdr->e_type);
+  printf("machine: %hu\n", ehdr->e_machine);
+  printf("sections number: %hu\n", ehdr->e_shnum);
 }
 
 ObjectFile load_object(const char *file_name) {
   ObjectFile obj_file;
 
-  obj_file.file_name = file_name;
+  int fd = open(file_name, O_RDONLY);
+
+  if (fd < 0) {
+    perror("open");
+    exit(1);
+  }
+
+  obj_file.file_name = (char *)file_name;
+
   obj_file.size = get_file_size(file_name);
 
-  // get elf headers
+  obj_file.data = mmap(NULL, obj_file.size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+  obj_file.ehdr = (Elf64_Ehdr *)obj_file.data;
+  print_elf_header(obj_file.ehdr);
+  printf("%s: %zu", obj_file.file_name, obj_file.size);
 
   // get section headers
 
   return obj_file;
-};
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
